ea0f8116e2e67a8bc06be9bf22e21194cb00afc7
Niels <nbijl@petersplanet.nl>
2021-02-08 17:10:18 +0100

    last test added

--- Perceptron/tests/testPerceptronNetwork.py
+++ Perceptron/tests/testPerceptronNetwork.py
@@ -5,11 +5,12 @@ from Perceptron.PerceptronLayer import *
 
 
 class TestPerceptronNetwork(unittest.TestCase):
-    def setUp(self):
+    def testPerceptronNetworkXOR(self):
         """
-        This function gets executed before every test function gets executed
-        It creates a XOR perceptron network
+        This functions tests every possible input of the XOR perceptron network.
         """
+
+        """ -----Create XOR network----- """
 
         """ Create First hidden layer """
         orPerceptron = Perceptron(weights=[1, 1], bias=1)
@@ -23,16 +24,12 @@ class TestPerceptronNetwork(unittest.TestCase):
         """ Create perceptron network """
         self.xorNetwork = PerceptronNetwork(layers=[firstLayer, secondLayer])
 
-    def testPerceptronNetwork(self):
-        """
-        This functions tests every possible input of the XOR perceptron network.
-        """
-
+        """ ------Test every possible input----- """
         inputNetwork = [[[1, 1], 0],
                         [[1, 0], 1],
                         [[0, 1], 1],
                         [[0, 0], 0]]
-        """ Test every possible input """
+
         for testInput in inputNetwork:
             """ Set the input for the perceptron network """
             self.xorNetwork.setInput(networkInput=testInput[0])
@@ -43,5 +40,38 @@ class TestPerceptronNetwork(unittest.TestCase):
             """ Test of the output is correct """
             self.assertEqual(self.xorNetwork.output[0], testInput[1])
 
+    def testPerceptronNetworkHalfAdder(self):
+        """
+        This functions tests every possible input of the half adder perceptron network.
+        """
 
+        """ -----Create half adder network----- """
 
+        """ Create First hidden layer """
+        orPerceptron = Perceptron(weights=[1, 1], bias=1)
+        nandPerceptron = Perceptron(weights=[-1, -1], bias=-1)
+        andPerceptron = Perceptron(weights=[1, 1], bias=2)
+        firstLayer = PerceptronLayer(perceptrons=[orPerceptron, nandPerceptron, andPerceptron])
+
+        """ Create second hidden layer """
+        andPerceptron = Perceptron(weights=[1, 1, 0], bias=2)
+        extraPerceptron = Perceptron(weights=[0, 0, 1], bias=1)
+        secondLayer = PerceptronLayer(perceptrons=[andPerceptron, extraPerceptron])
+
+        """ Create perceptron network """
+        self.halfAdderNetwork = PerceptronNetwork(layers=[firstLayer, secondLayer])
+
+        """ ------Test every possible input----- """
+        inputNetwork = [[[1, 1], [0, 1]],  # [[input], [output]]
+                        [[1, 0], [1, 0]],
+                        [[0, 1], [1, 0]],
+                        [[0, 0], [0, 0]]]
+        for testInput in inputNetwork:
+            """ Set the input for the perceptron network """
+            self.halfAdderNetwork.setInput(networkInput=testInput[0])
+
+            """ Run perceptron network """
+            self.halfAdderNetwork.feedForward()
+
+            """ Test of the output is correct """
+            self.assertEqual(self.halfAdderNetwork.output, testInput[1])
